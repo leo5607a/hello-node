794210a12e9fa66e45a2355fea26c3ec0077db3f
leo5607a <leo5607a@gmail.com>
2021-10-17 11:04:20 +0800

    爬蟲第二版(await)

--- crawler/app.js
+++ crawler/app.js
@@ -4,17 +4,30 @@ let stockCode = "2330";
 let date = "20211017";
 let format = "json";
 
-axios
-  .get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
-    params: {
-      response: format,
-      date: date,
-      stockNo: stockCode,
-    },
-  })
-  .then((res) => {
-    console.log(res.data);
-  })
-  .catch((e) => {
-    console.error("發生錯誤啦", e);
-  });
+// axios
+//   .get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
+//     params: {
+//       response: format,
+//       date: date,
+//       stockNo: stockCode,
+//     },
+//   })
+//   .then((res) => {
+//     console.log(res.data);
+//   })
+//   .catch((e) => {
+//     console.error("發生錯誤啦", e);
+//   });
+
+async function get(){
+    let get = await axios
+    .get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
+      params: {
+        response: format,
+        date: date,
+        stockNo: stockCode,
+      },
+    })
+    console.log(get.data);
+}
+get()
